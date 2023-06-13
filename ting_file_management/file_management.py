147db0a468562e5ff8eeb3de634b641d551b10f1
kayo <kayohramos@gmail.com>
2023-06-13 17:07:52 -0300

    feat: req02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print('Formato inválido', file=sys.stderr)
+    try:
+        with open(path_file, 'r') as file:
+            # print(file.read())
+            content = file.read().split('\n')
+            print(type(content))
+            return content
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
