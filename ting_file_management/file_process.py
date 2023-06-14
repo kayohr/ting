cabc7a05360fb0992df4db42443e24ac90af32d1
kayo <kayohramos@gmail.com>
2023-06-14 12:16:26 -0300

    feat:req05

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -29,4 +29,8 @@ def remove(instance):
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        file_data = instance.search(position)
+        print(file_data, file=sys.stdout)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
